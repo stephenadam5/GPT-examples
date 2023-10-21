a0fc1e79b64f66617453134df15d826139e41415
Stephen Adams <81650373+stephenadam5@users.noreply.github.com>
2023-10-21 12:45:00 +0100

    Update f1_openai_analytics.py

--- f1_openai_analytics.py
+++ f1_openai_analytics.py
@@ -1,14 +1,32 @@
 # WORK IN PROGRESS
 
-import socket
+#---------------------------------------------
 
-from f1_2020_telemetry.packets import unpack_udp_packet
+# With f1_2020_telemetry library:
 
-udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-udp_socket.bind(("", 20777))
+# import socket
 
-while True:
-    udp_packet = udp_socket.recv(2048)
-    packet = unpack_udp_packet(udp_packet)
-    print("Received:", packet)
-    print()
+# from f1_2020_telemetry.packets import unpack_udp_packet
+
+# udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# udp_socket.bind(("", 20777))
+
+# while True:
+#     udp_packet = udp_socket.recv(2048)
+#     packet = unpack_udp_packet(udp_packet)
+#     print("Received:", packet)
+#     print()
+
+#---------------------------------------------
+
+# Without f1_2020_telemetry library (needs unpacking):
+
+# import socket
+
+# udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# udp_socket.bind(("", 20777))
+
+# while True:
+#     udp_packet = udp_socket.recv(2048)
+#     print("Received:", udp_packet)
+#     print()
